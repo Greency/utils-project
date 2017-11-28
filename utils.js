42b7be24f319ec8cd52aaa1decec359e1fad6409
Greency <1015561170@qq.com>
2017-11-28 09:48:27 +0800

    add Function formatNumber

---
+++ utils.js
@@ -0,0 +1,22 @@
+module.exports = {
+  /**
+   * 格式化为价格，如 ￥12.99
+   * @param number|string digit 被格式化的原数
+   * @param 
+   * @return string 格式化后的数
+   */
+  formatNumber(digit, places = 2) {
+    var _digit = '' + digit,
+      _index = _digit.indexOf('.') // “ . ”的索引
+    if (_index === -1) {
+      return _digit + '.00'
+    } else {
+      var _tempStr = _digit.substring(_index, _digit.length)
+      if (_tempStr.length === 2) {
+        return _digit + '0'
+      } else {
+        return _digit.substring(0, _index) + _tempStr.substring(0, places + 1)
+      }
+    }
+  }
+}
