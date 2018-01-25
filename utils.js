902ab0c75600ca6f94b090cf7b09bbbcdb2a009e
陈勇 <1015561170@qq.com>
2018-01-25 15:48:22 +0800

    Update utils.js
    add utf16toEntities,changeToUtf16Str

--- utils.js
+++ utils.js
@@ -5,7 +5,7 @@ module.exports = {
    * @param number places 保留的位数
    * @return string 格式化后的数
    */
-  formatNumber(digit, places = 2) {
+  formatNumber: function(digit, places = 2) {
     var _digit = '' + digit,
       _index = _digit.indexOf('.') // “ . ”的索引
     if (_index === -1) {
@@ -18,5 +18,42 @@ module.exports = {
         return _digit.substring(0, _index) + _tempStr.substring(0, places + 1)
       }
     }
-  }
+  },
+  /*
+	 *用户移动端emoji表情在数据库中的存储
+	 *用于把用utf16编码的字符转换成实体字符，以供后台存储 
+	 *@param  {String} str 将要转换的字符串，其中含有utf16字符将被自动检出 
+	 *@return {String} str 转换后的字符串，utf16字符将被转换成&#xxxx;形式的实体字符 
+	 * */
+	utf16toEntities: function(str) {
+		var reg = /[\ud800-\udbff][\udc00-\udfff]/g,
+		str = str.replace(reg, function(char) {
+			var H, L, code
+			if(char.length === 2) {
+				H = char.charCodeAt(0)
+				L = char.charCodeAt(1)
+				code = (H - 0xD800) * 0x400 + 0x10000 + L - 0xDC00
+				return "&#" + code + ";"
+			} else {
+				return char
+			}
+		})
+		return str
+	},
+	/*
+	 *&#xxxxxx;形式的字符串存入数据库后
+	 *读出来的形式为 &amp;#xxxxxx;
+	 *此种格式html解析不出来原有的符号
+	 *&被转成了&amp;
+	 *此方法是将字符串中的&amp;转成&
+	 *@param {String} str 未被转换的字符串
+	 *@return {String} str 转换后的字符串
+	 * */
+	changeToUtf16Str: function(str){
+		var reg = /&amp;/g,
+		str = str.replace(reg, function(char) {
+			return '&'
+		})
+		return str
+	}
 }
