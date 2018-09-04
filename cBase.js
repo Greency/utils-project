35f372036eef7d0acda09ca14f3a4de01166f164
Greency <1015561170@qq.com>
2018-09-04 21:50:00 +0800

    add [fun]isEmpty

--- cBase.js
+++ cBase.js
@@ -1,9 +1,32 @@
 var b = (function(w){
+
   function existPro(obj, pro){
     return obj.hasOwnProperty(pro);
   }
   
-  return {
-    existPro,
+  function isEmpty(val){
+    console.log('val: ', val === '', '  typeof', typeof val === 'string' );
+
+    if(val === undefined || val === 'undefined' || val === null) return true;
+    
+    if(typeof val === 'string'){
+      if(val.trim() === '') return true;
+    }
+    if(val instanceof Array){
+      if(val.length === 0) return true;
+    }
+
+    if(typeof val === 'object'){
+      if(JSON.stringify(val) === '{}') return true;
+      for(var key in val){
+        if(isEmpty(val[key])) return true;
+      }
+    }
+    return false;
   }
-})(Window);
+
+  return {
+    existPro: existPro,
+    isEmpty: isEmpty
+  }
+})(window);
