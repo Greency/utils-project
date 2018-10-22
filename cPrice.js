09eef753274c37c49edc921985b788ec0382b70b
chenyong <1015561170@qq.com>
2018-10-22 09:26:07 +0800

    feat: 简化代码

--- cPrice.js
+++ cPrice.js
@@ -1,24 +1,30 @@
 const cPrice = {
-    calculate(type, a, b){
+    calculate(type, a, b) {
+        let res = 0;
         a = parseFloat((a * 100).toFixed(10));
         b = parseFloat((b * 100).toFixed(10));
-        switch(type){
-            case '+': return this.formatePrice(( a + b ) / 100);break;
-            case '*': return this.formatePrice(( a * b ) / 10000);break;
+        switch (type) {
+          case '+':
+            res = (a + b) / 100;
+            break;
+          case '*':
+            res = (a * b) / 10000;
+            break;
         }
+        return this.formatePrice(res);
     },
-    formatePrice(val){
+    formatePrice(val) {
         val = val.toString();
         let len = val.length,
-            pointIndex = val.indexOf('.');
-        if(pointIndex === -1) return `${val}.00`;
-        if(len - (pointIndex + 1) === 1) return `${val}0`;
+          pointIndex = val.indexOf('.');
+        if (pointIndex === -1) return `${val}.00`;
+        if (len - (pointIndex + 1) === 1) return `${val}0`;
         return val;
     },
-    add(a, b){
+    add(a, b) {
         return this.calculate('+', a, b);
     },
-    x(a, b){
+    x(a, b) {
         return this.calculate('*', a, b);
     }
 }
